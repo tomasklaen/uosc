961c88db5dce5594975c53a000e3bb0395a8dfa4
Christoph Heinrich <christoph.heinrich@student.tugraz.at>
2022-07-30 00:14:09 +0200

    Fix menu crash when quickly repeatedly opening and closing submenu
    Opening and closing a submenu very quickly very quickly
    (smashing right and left arrow as fast as I possibly can)
    resulted in a nil error.

--- uosc.lua
+++ uosc.lua
@@ -1068,7 +1068,7 @@ function Menu:open(items, open_item, opts)
 					elements:add('menu', transition_target)
 				end
 				menu.transition = nil
-				transition_target:back()
+				if transition_target then transition_target:back() end
 				return
 			else
 				menu.transition = {to = 'parent', target = this.parent_menu}
@@ -1100,7 +1100,7 @@ function Menu:open(items, open_item, opts)
 				local target = menu.transition.target
 				tween_element_stop(target)
 				menu.transition = nil
-				target:open_selected_item(soft)
+				if target then target:open_selected_item(soft) end
 				return
 			end
 
