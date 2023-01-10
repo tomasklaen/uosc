e6b1fc94031c606b7974843b2e9386bf5de773a3
Christoph Heinrich <christoph.heinrich@student.tugraz.at>
2023-01-10 19:54:14 +0100

    tweak: buffered time threshold based on displayed value

--- scripts/uosc_shared/elements/Timeline.lua
+++ scripts/uosc_shared/elements/Timeline.lua
@@ -219,14 +219,14 @@ function Timeline:render()
 	ass:rect(fax, fay, fbx, fby, {opacity = options.timeline_opacity})
 
 	-- Uncached ranges
-	local buffered_time = nil
+	local buffered_playtime = nil
 	if state.uncached_ranges then
 		local opts = {size = 80, anchor_y = fby}
 		local texture_char = visibility > 0 and 'b' or 'a'
 		local offset = opts.size / (visibility > 0 and 24 or 28)
 		for _, range in ipairs(state.uncached_ranges) do
-			if not buffered_time and (range[1] > state.time or range[2] > state.time) then
-				buffered_time = range[1] - state.time
+			if not buffered_playtime and (range[1] > state.time or range[2] > state.time) then
+				buffered_playtime = (range[1] - state.time) / (state.speed or 1)
 			end
 			if options.timeline_cache then
 				local ax = range[1] < 0.5 and bax or math.floor(t2x(range[1]))
@@ -316,10 +316,11 @@ function Timeline:render()
 	if text_opacity > 0 then
 		local time_opts = {size = self.font_size, opacity = text_opacity, border = 2}
 		-- Upcoming cache time
-		if buffered_time and options.buffered_time_threshold > 0 and buffered_time < options.buffered_time_threshold then
+		if buffered_playtime and options.buffered_time_threshold > 0
+			and buffered_playtime < options.buffered_time_threshold then
 			local x, align = fbx + 5, 4
 			local cache_opts = {size = self.font_size * 0.8, opacity = text_opacity * 0.6, border = 1}
-			local human = round(math.max(buffered_time / (state.speed or 1), 0)) .. 's'
+			local human = round(math.max(buffered_playtime, 0)) .. 's'
 			local width = text_width(human, cache_opts)
 			local time_width = text_width('00:00:00', time_opts)
 			local min_x, max_x = bax + spacing + 5 + time_width, bbx - spacing - 5 - time_width
