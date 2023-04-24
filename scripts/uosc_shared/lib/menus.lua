fd05d67a7db66df9764431151b01b238833fe07a
Eva <evauwu@riseup.net>
2023-04-24 23:38:20 +0200

    fix: translate audio channels and devices list

--- scripts/uosc_shared/lib/menus.lua
+++ scripts/uosc_shared/lib/menus.lua
@@ -106,7 +106,7 @@ function create_select_tracklist_type_menu_opener(menu_title, track_type, track_
 				end
 				if track['demux-fps'] then h(string.format('%.5gfps', track['demux-fps'])) end
 				h(track.codec)
-				if track['audio-channels'] then h(track['audio-channels'] .. ' channels') end
+				if track['audio-channels'] then h(t('%d channels', track['audio-channels'])) end
 				if track['demux-samplerate'] then h(string.format('%.3gkHz', track['demux-samplerate'] / 1000)) end
 				if track.forced then h(t('forced')) end
 				if track.default then h(t('default')) end
