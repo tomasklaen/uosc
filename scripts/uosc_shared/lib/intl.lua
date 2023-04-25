cbbb40611e0f04940effbb68807804d0e9cb3681
natural-harmonia-gropius <50797982+natural-harmonia-gropius@users.noreply.github.com>
2023-04-25 15:39:00 +0800

    fix: `en` locale not working (#526)

--- scripts/uosc_shared/lib/intl.lua
+++ scripts/uosc_shared/lib/intl.lua
@@ -44,6 +44,8 @@ function make_locale()
 		lang = languages[i]
 		if (lang:match('.json$')) then
 			table_assign(translations, get_locale_from_json(lang))
+		elseif (lang == 'en') then
+			translations = {}
 		else
 			table_assign(translations, get_locale_from_json('~~/scripts/uosc_shared/intl/' .. lang:lower() .. '.json'))
 		end
