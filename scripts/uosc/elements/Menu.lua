e783ad1f133e06a50d424291143d25497fbecfdd
Eva <42466980+po5@users.noreply.github.com>
2023-07-26 23:20:09 +0200

    fix: crash when a script menu update renders selected index unavailable (#591)
    Co-authored-by: Christoph Heinrich <christoph.heinrich@student.tugraz.at>

--- scripts/uosc/elements/Menu.lua
+++ scripts/uosc/elements/Menu.lua
@@ -186,6 +186,10 @@ function Menu:update(data)
 		local old_menu = self.by_id[menu.is_root and '__root__' or menu.id]
 		if old_menu then table_assign(menu, old_menu, {'selected_index', 'scroll_y', 'fling'}) end
 
+		if menu.selected_index then
+			menu.selected_index = #menu.items > 0 and clamp(1, menu.selected_index, #menu.items) or nil
+		end
+
 		new_all[#new_all + 1] = menu
 		new_by_id[menu.is_root and '__root__' or menu.id] = menu
 	end
