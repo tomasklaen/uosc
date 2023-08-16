68b5b2aaed05ee9e7f07decad89cbeeff9bc67ed
tomasklaen <tomas.klaen@gmail.com>
2023-08-16 12:52:07 +0200

    refactor: improved protocol matching patterns
    ref #605

--- scripts/uosc/main.lua
+++ scripts/uosc/main.lua
@@ -969,7 +969,7 @@ bind_command('playlist', create_self_updating_menu_opener({
 	serializer = function(playlist)
 		local items = {}
 		for index, item in ipairs(playlist) do
-			local is_url = item.filename:find('://')
+			local is_url = is_protocol(item.filename)
 			local item_title = type(item.title) == 'string' and #item.title > 0 and item.title or false
 			items[index] = {
 				title = item_title or (is_url and item.filename or serialize_path(item.filename).basename),
