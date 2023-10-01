6480ad2f48d9b3f7080787c4fc976af057236163
tomasklaen <tomas.klaen@gmail.com>
2023-10-01 20:27:47 +0200

    only grow width while searching

--- scripts/uosc/elements/Menu.lua
+++ scripts/uosc/elements/Menu.lua
@@ -11,7 +11,7 @@ local Element = require('elements/Element')
 ---@alias MenuStackItem MenuStackValue|MenuStack
 ---@alias MenuStackValue {title?: string; hint?: string; icon?: string; value: any; active?: boolean; bold?: boolean; italic?: boolean; muted?: boolean; keep_open?: boolean; separator?: boolean; selectable?: boolean; align?: 'left'|'center'|'right'; title_width: number; hint_width: number}
 ---@alias Fling {y: number, distance: number, time: number, easing: fun(x: number), duration: number, update_cursor?: boolean}
----@alias Search {query: string; timeout: unknown; min_top: number; source: {width: number; top: number; scroll_y: number; selected_index?: integer; items?: MenuDataItem[]}}
+---@alias Search {query: string; timeout: unknown; min_top: number; max_width: number; source: {width: number; top: number; scroll_y: number; selected_index?: integer; items?: MenuDataItem[]}}
 
 ---@alias Modifiers {shift?: boolean, ctrl?: boolean, alt?: boolean}
 ---@alias MenuCallbackMeta {modifiers: Modifiers}
@@ -299,7 +299,7 @@ function Menu:update_dimensions()
 	local height_available = display.height - margin * 2
 
 	for _, menu in ipairs(self.all) do
-		local width = math.max(menu.search and menu.search.source.width or 0, menu.max_width)
+		local width = math.max(menu.search and menu.search.max_width or 0, menu.max_width)
 		menu.width = round(clamp(min_width, width, display.width - margin * 2))
 		local title_height = (menu.is_root and menu.title or menu.search) and self.scroll_step or 0
 		local max_height = height_available - title_height
@@ -310,7 +310,10 @@ function Menu:update_dimensions()
 			menu.search and math.min(menu.search.min_top, menu.search.source.top) or height_available,
 			round((height_available - menu.height + title_height) / 2)
 		)
-		if menu.search then menu.search.min_top = math.min(menu.search.min_top, menu.top) end
+		if menu.search then
+			menu.search.min_top = math.min(menu.search.min_top, menu.top)
+			menu.search.max_width = math.max(menu.search.max_width, menu.width)
+		end
 		menu.scroll_height = math.max(content_height - menu.height - self.item_spacing, 0)
 		menu.scroll_y = menu.scroll_y or 0
 		self:scroll_to(menu.scroll_y, menu) -- clamps scroll_y to scroll limits
@@ -815,7 +818,7 @@ function Menu:search_init(menu)
 		timeout:kill()
 	end
 	menu.search = {
-		query = '', timeout = timeout, min_top = menu.top,
+		query = '', timeout = timeout, min_top = menu.top, max_width = menu.width,
 		source = {
 			width = menu.width, top = menu.top,
 			scroll_y = menu.scroll_y, selected_index = menu.selected_index,
