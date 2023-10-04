tomasklaen/uosc

commit 7f9a8cae6df38ca6a9ba9327d1a0f0f143ba346d

feat: reworked timeline progress mode (#661)
* feat: reworked timeline progress mode

Config options:

```
timeline_line_width_fullscreen=3
timeline_line_width_minimized_scale=10
timeline_size_min=2
timeline_size_max=40
timeline_size_min_fullscreen=0
timeline_size_max_fullscreen=60
timeline_start_hidden=no
```

have been replaced with:

```
timeline_size=40
timeline_size_fullscreen=60
progress=windowed
progress_size=2
progress_line_width=20
```

This simplifies stuff a bunch, and enables timeline's progress mode to be togglable at all times. Previously you could only toggle when it was configured to be visible in current context.

closes #630

scripts/uosc/elements/Timeline.lua
@@ -9,13 +9,14 @@ function Timeline:init()
 	---@type false|{pause: boolean, distance: number, last: {x: number, y: number}}
 	self.pressed = false
 	self.obstructed = false
-	self.size_max = 0
-	self.size_min = 0
-	self.size_min_override = options.timeline_start_hidden and 0 or nil
+	self.size = 0
+	self.progress_size = 0
 	self.font_size = 0
 	self.top_border = options.timeline_border
 	self.is_hovered = false
 	self.has_thumbnail = false
+
+	self:decide_progress_size()
 
 	-- Release any dragging when file gets unloaded
 	mp.register_event('end-file', function() self.pressed = false end)
@@ -32,14 +33,9 @@ function Timeline:decide_enabled()
 	if self.enabled ~= previous then Elements:trigger('timeline_enabled', self.enabled) end
 end
 
-function Timeline:get_effective_size_min()
-	return self.size_min_override or self.size_min
-end
-
 function Timeline:get_effective_size()
-	if Elements.speed and Elements.speed.dragging then return self.size_max end
-	local size_min = self:get_effective_size_min()
-	return size_min + math.ceil((self.size_max - size_min) * self:get_visibility())
+	if Elements.speed and Elements.speed.dragging then return self.size end
+	return self.progress_size + math.ceil((self.size - self.progress_size) * self:get_visibility())
 end
 
 function Timeline:get_effective_line_width()
@@ -49,16 +45,10 @@ end
 function Timeline:get_is_hovered() return self.enabled and self.is_hovered end
 
 function Timeline:update_dimensions()
-	if state.fullormaxed then
-		self.size_min = options.timeline_size_min_fullscreen
-		self.size_max = options.timeline_size_max_fullscreen
-	else
-		self.size_min = options.timeline_size_min
-		self.size_max = options.timeline_size_max
-	end
-	self.font_size = math.floor(math.min((self.size_max + 60) * 0.2, self.size_max * 0.96) * options.font_scale)
+	self.size = state.fullormaxed and options.timeline_size_fullscreen or options.timeline_size
+	self.font_size = math.floor(math.min((self.size + 60) * 0.2, self.size * 0.96) * options.font_scale)
 	self.ax = Elements.window_border.size
-	self.ay = display.height - Elements.window_border.size - self.size_max - self.top_border
+	self.ay = display.height - Elements.window_border.size - self.size - self.top_border
 	self.bx = display.width - Elements.window_border.size
 	self.by = display.height - Elements.window_border.size
 	self.width = self.bx - self.ax
@@ -68,12 +58,25 @@ function Timeline:update_dimensions()
 	-- Disable if not enough space
 	local available_space = display.height - Elements.window_border.size * 2
 	if Elements.top_bar.enabled then available_space = available_space - Elements.top_bar.size end
-	self.obstructed = available_space < self.size_max + 10
+	self.obstructed = available_space < self.size + 10
 	self:decide_enabled()
 end
 
+function Timeline:decide_progress_size()
+	local show = options.progress == 'always'
+		or (options.progress == 'fullscreen' and state.fullormaxed)
+		or (options.progress == 'windowed' and not state.fullormaxed)
+	self.progress_size = show and options.progress_size or 0
+end
+
+function Timeline:toggle_progress()
+	local current = self.progress_size
+	self:tween_property('progress_size', current, current > 0 and 0 or options.progress_size)
+	request_render()
+end
+
 function Timeline:get_time_at_x(x)
-	local line_width = (options.timeline_style == 'line' and self:get_effective_line_width() - 1 or 0)
+	local line_width = (options.timeline_style == 'line' and options.timeline_line_width - 1 or 0)
 	local time_width = self.width - line_width - 1
 	local fax = (time_width) * state.time / state.duration
 	local fbx = fax + line_width
@@ -107,7 +110,10 @@ function Timeline:on_prop_duration() self:decide_enabled() end
 function Timeline:on_prop_time() self:decide_enabled() end
 function Timeline:on_prop_border() self:update_dimensions() end
 function Timeline:on_prop_title_bar() self:update_dimensions() end
-function Timeline:on_prop_fullormaxed() self:update_dimensions() end
+function Timeline:on_prop_fullormaxed()
+	self:decide_progress_size()
+	self:update_dimensions()
+end
 function Timeline:on_display() self:update_dimensions() end
 function Timeline:on_options()
 	self.top_border = options.timeline_border
@@ -136,9 +142,8 @@ function Timeline:handle_wheel_up() mp.commandv('seek', options.timeline_step) e
 function Timeline:handle_wheel_down() mp.commandv('seek', -options.timeline_step) end
 
 function Timeline:render()
-	if self.size_max == 0 then return end
+	if self.size == 0 then return end
 
-	local size_min = self:get_effective_size_min()
 	local size = self:get_effective_size()
 	local visibility = self:get_visibility()
 	self.is_hovered = false
@@ -161,8 +166,8 @@ function Timeline:render()
 
 	local ass = assdraw.ass_new()
 
-	-- Text opacity rapidly drops to 0 just before it starts overflowing, or before it reaches timeline.size_min
-	local hide_text_below = math.max(self.font_size * 0.8, size_min * 2)
+	-- Text opacity rapidly drops to 0 just before it starts overflowing, or before it reaches self.progress_size
+	local hide_text_below = math.max(self.font_size * 0.8, self.progress_size * 2)
 	local hide_text_ramp = hide_text_below / 2
 	local text_opacity = clamp(0, size - hide_text_below, hide_text_ramp) / hide_text_ramp
 
@@ -170,7 +175,7 @@ function Timeline:render()
 	local tooltip_margin = 10
 	local timestamp_gap = tooltip_gap
 
-	local spacing = math.max(math.floor((self.size_max - self.font_size) / 2.5), 4)
+	local spacing = math.max(math.floor((self.size - self.font_size) / 2.5), 4)
 	local progress = state.time / state.duration
 	local is_line = options.timeline_style == 'line'
 
@@ -182,12 +187,10 @@ function Timeline:render()
 	local line_width = 0
 
 	if is_line then
-		local minimized_fraction = 1 - math.min((size - size_min) / ((self.size_max - size_min) / 8), 1)
-		local line_width_max = self:get_effective_line_width()
-		local max_min_width_delta = size_min > 0
-			and line_width_max - line_width_max * options.timeline_line_width_minimized_scale
-			or 0
-		line_width = line_width_max - (max_min_width_delta * minimized_fraction)
+		local minimized_fraction = 1 - math.min((size - self.progress_size) / ((self.size - self.progress_size) / 8), 1)
+		local line_width_normal = self:get_effective_line_width()
+		local progress_delta = self.progress_size > 0 and options.progress_line_width - line_width_normal or 0
+		line_width = line_width_normal - (progress_delta * minimized_fraction)
 		fax = bax + (self.width - line_width) * progress
 		fbx = fax + line_width
 		line_width = line_width - 1

script-opts/uosc.conf
@@ -2,19 +2,10 @@
 timeline_style=line
 # Line display style config
 timeline_line_width=2
-timeline_line_width_fullscreen=3
-# Scale the width of the line when minimized (timeline_size_min)
-timeline_line_width_minimized_scale=10
-# Timeline size when minimized, 0 will hide it completely
-timeline_size_min=2
 # Timeline size when fully expanded, in pixels, 0 to disable
-timeline_size_max=40
-# Same as ^ but when in fullscreen
-timeline_size_min_fullscreen=0
-timeline_size_max_fullscreen=60
-# Same thing as calling toggle-progress command once on startup
-timeline_start_hidden=no
-# Comma separated states when timeline should always be visible. available: paused, audio, image, video, idle
+timeline_size=40
+timeline_size_fullscreen=60
+# Comma separated states when timeline should always be fully visible. available: paused, audio, image, video, idle
 timeline_persistency=paused
 # Timeline opacity
 timeline_opacity=0.9
@@ -26,6 +17,12 @@ timeline_step=5
 timeline_chapters_opacity=0.8
 # Render cache indicators for streaming content
 timeline_cache=yes
+
+# When to display an always visible progress bar (minimized timeline). Can be: windowed, fullscreen, always, never
+# Can also be toggled on demand with `toggle-progress` command.
+progress=windowed
+progress_size=2
+progress_line_width=20
 
 # A comma delimited list of items to construct the controls bar above the timeline. Set to `never` to disable.
 # Parameter spec: enclosed in `{}` means value, enclosed in `[]` means optional

scripts/uosc/elements/Controls.lua
@@ -232,7 +232,7 @@ function Controls:update_dimensions()
 	-- Disable when not enough space
 	local available_space = display.height - Elements.window_border.size * 2
 	if Elements.top_bar.enabled then available_space = available_space - Elements.top_bar.size end
-	if Elements.timeline.enabled then available_space = available_space - Elements.timeline.size_max end
+	if Elements.timeline.enabled then available_space = available_space - Elements.timeline.size end
 	self.enabled = available_space > size + 10
 
 	-- Reset hide/enabled flags

scripts/uosc/main.lua
@@ -17,12 +17,11 @@ defaults = {
 	timeline_style = 'line',
 	timeline_line_width = 2,
 	timeline_line_width_fullscreen = 3,
-	timeline_line_width_minimized_scale = 10,
-	timeline_size_min = 2,
-	timeline_size_max = 40,
-	timeline_size_min_fullscreen = 0,
-	timeline_size_max_fullscreen = 60,
-	timeline_start_hidden = false,
+	timeline_size = 40,
+	timeline_size_fullscreen = 60,
+	progress = 'windowed',
+	progress_size = 2,
+	progress_line_width = 20,
 	timeline_persistency = 'paused',
 	timeline_opacity = 0.9,
 	timeline_border = 1,
@@ -949,16 +948,7 @@ bind_command('flash-top-bar', function() Elements:flash({'top_bar'}) end)
 bind_command('flash-volume', function() Elements:flash({'volume'}) end)
 bind_command('flash-speed', function() Elements:flash({'speed'}) end)
 bind_command('flash-pause-indicator', function() Elements:flash({'pause_indicator'}) end)
-bind_command('toggle-progress', function()
-	local timeline = Elements.timeline
-	if timeline.size_min_override then
-		timeline:tween_property('size_min_override', timeline.size_min_override, timeline.size_min, function()
-			timeline.size_min_override = nil
-		end)
-	else
-		timeline:tween_property('size_min_override', timeline.size_min, 0)
-	end
-end)
+bind_command('toggle-progress', function() Elements.timeline:toggle_progress() end)
 bind_command('toggle-title', function() Elements.top_bar:toggle_title() end)
 bind_command('decide-pause-indicator', function() Elements.pause_indicator:decide() end)
 bind_command('menu', function() toggle_menu_with_items() end)

commit e7371f61e0814d9231b02621e83347aa4b83de55

fix: progress line width broken in #661
ref #661

scripts/uosc/elements/Timeline.lua
@@ -190,7 +190,7 @@ function Timeline:render()
 		local minimized_fraction = 1 - math.min((size - self.progress_size) / ((self.size - self.progress_size) / 8), 1)
 		local line_width_normal = self:get_effective_line_width()
 		local progress_delta = self.progress_size > 0 and options.progress_line_width - line_width_normal or 0
-		line_width = line_width_normal - (progress_delta * minimized_fraction)
+		line_width = line_width_normal + (progress_delta * minimized_fraction)
 		fax = bax + (self.width - line_width) * progress
 		fbx = fax + line_width
 		line_width = line_width - 1