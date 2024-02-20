2370cb4e1d54b6b6b52c639727fd7c37815b5caa
tomasklaen <tomas.klaen@gmail.com>
2024-02-20 09:17:48 +0100

    fix: disabling chapter indicators now disables chapter tooltips as well
    resolves #849

--- src/uosc/elements/Timeline.lua
+++ src/uosc/elements/Timeline.lua
@@ -455,7 +455,7 @@ function Timeline:render()
 		end
 
 		-- Chapter title
-		if #state.chapters > 0 then
+		if config.opacity.chapters > 0 and #state.chapters > 0 then
 			local _, chapter = itable_find(state.chapters, function(c) return hovered_seconds >= c.time end,
 				#state.chapters, 1)
 			if chapter and not chapter.is_end_only then
