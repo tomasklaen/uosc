6ef8af59ea5dddcb34df6fb392bfd2d2fc2663f5
tomasklaen <tomas.klaen@gmail.com>
2024-09-06 11:34:49 +0200

    fix: getting unsuported formats from clipboard returning unhelpful error message
    ref #983

--- src/ziggy/commands/clipboard.go
+++ src/ziggy/commands/clipboard.go
@@ -1,8 +1,10 @@
 package commands
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"strings"
 	"uosc/bins/src/ziggy/lib"
 
 	"github.com/atotto/clipboard"
@@ -13,8 +15,18 @@ type ClipboardResult struct {
 }
 
 func GetClipboard(_ []string) {
+	// We need to do this instead of just `Payload: lib.Must(clipboard.ReadAll())` because
+	// the atotto/clipboard returns unhelpful messages like "the operation completed successfully".
+	payload, err := clipboard.ReadAll()
+	if err != nil {
+		if strings.Contains(strings.ToLower(err.Error()), "successfully") {
+			lib.Check(errors.New("clipboard format not supported"))
+		}
+		lib.Check(err)
+	}
+
 	fmt.Print(string(lib.Must(lib.JSONMarshal(ClipboardResult{
-		Payload: lib.Must(clipboard.ReadAll()),
+		Payload: payload,
 	}))))
 }
 
