tomasklaen/uosc

commit be52ba27945a70f590d4ffa4de193b59fe210534

fix: UI sometimes staying visible when cursor left the window
closes #1000

src/uosc/lib/cursor.lua
@@ -5,7 +5,7 @@ local cursor = {
 	y = math.huge,
 	hidden = true,
 	distance = 0, -- Distance traveled during current move. Reset by `cursor.distance_reset_timer`.
-	hover_raw = false,
+	last_hover = false, -- Stores `mouse.hover` boolean of the last mouse event for enter/leave detection.
 	-- Event handlers that are only fired on zones defined during render loop.
 	---@type {event: string, hitbox: Hitbox; handler: CursorEventHandler}[]
 	zones = {},
@@ -401,12 +401,12 @@ end
 -- Movement
 function handle_mouse_pos(_, mouse)
 	if not mouse then return end
-	if cursor.hover_raw and not mouse.hover then
+	if cursor.last_hover and not mouse.hover then
 		cursor:leave()
-	else
+	elseif not cursor.hidden or not cursor.last_hover and mouse.hover then
 		cursor:move(mouse.x, mouse.y)
 	end
-	cursor.hover_raw = mouse.hover
+	cursor.last_hover = mouse.hover
 end
 mp.observe_property('mouse-pos', 'native', handle_mouse_pos)
 

commit 483bfa8bd8a43a16da60fd58777c1ee56ec7ec66

fix: UI cursor proximity broken in fullscreen
A bug introduced by be52ba27945a70f590d4ffa4de193b59fe210534.

closes #1016

src/uosc/lib/cursor.lua
@@ -403,7 +403,7 @@ function handle_mouse_pos(_, mouse)
 	if not mouse then return end
 	if cursor.last_hover and not mouse.hover then
 		cursor:leave()
-	elseif not cursor.hidden or not cursor.last_hover and mouse.hover then
+	elseif not (cursor.last_hover == false and mouse.hover == false) then -- filters out duplicate mouse out events
 		cursor:move(mouse.x, mouse.y)
 	end
 	cursor.last_hover = mouse.hover