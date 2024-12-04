tomasklaen/uosc

commit 3bd8aefcf96cf18274a3bddaf295f1dbec80aace

feat(api): custom menus can no longer prevent their closing
This was causing an unreconcilable scenario where uosc wouldn't know what to do when a menu requested to be open, while another potentially not closable one was still open.

Resulted in ghosts of some menus staying open in the background, eating keybinds.

closes #1025, ref #1026

src/uosc/elements/Menu.lua
@@ -32,7 +32,7 @@ local Menu = class(Element)
 ---@param callback MenuCallback
 ---@param opts? MenuOptions
 function Menu:open(data, callback, opts)
-	local open_menu = self:is_open()
+	local open_menu = Menu:is_open()
 	if open_menu then
 		open_menu.is_being_replaced = true
 		open_menu:close(true)
@@ -61,10 +61,20 @@ function Menu:close(immediate, callback)
 		end
 
 		local function close()
+			local on_close = menu.root.on_close -- removed in menu:destroy()
 			Elements:remove('menu') -- calls menu:destroy() under the hood
 			Elements:update_proximities()
 			cursor:queue_autohide()
+
+			-- Call :close() callback
 			if callback then callback() end
+
+			-- Call callbacks/events defined on menu config
+			local close_event = {type = 'close'}
+			if not on_close or menu:command_or_event(on_close, {}, close_event) ~= 'event' then
+				menu.callback(close_event)
+			end
+
 			request_render()
 		end
 
@@ -146,13 +156,6 @@ function Menu:destroy()
 		utils.shared_script_property_set('uosc-menu-type', nil)
 	end
 	mp.set_property_native('user-data/uosc/menu/type', nil)
-end
-
-function Menu:request_close()
-	local callback = self.root.on_close
-	if not callback or self:command_or_event(callback, {}, {type = 'close'}) ~= 'event' then
-		self:close()
-	end
 end
 
 ---@param data MenuData
@@ -682,7 +685,7 @@ function Menu:handle_cursor_down()
 		self.drag_last_y = cursor.y
 		self.current.fling = nil
 	else
-		self:request_close()
+		self:close()
 	end
 end
 
@@ -1115,7 +1118,7 @@ function Menu:handle_shortcut(shortcut, info)
 		if menu.search and menu.search_style ~= 'palette' then
 			self:search_cancel()
 		else
-			self:request_close()
+			self:close()
 		end
 	elseif id == 'left' and menu.parent_menu then
 		self:back()

src/uosc/lib/menus.lua
@@ -26,7 +26,7 @@ function open_command_menu(data, opts)
 			-- Convention: Only pure item activations should close the menu.
 			-- Using modifiers or triggering item actions should not.
 			if not event.keep_open and not event.modifiers and not event.action then
-				menu:request_close()
+				menu:close()
 			end
 		end
 	end
@@ -139,7 +139,6 @@ function create_self_updating_menu_opener(opts)
 			selected_index = selected_index,
 			on_move = opts.on_move and 'callback' or nil,
 			on_paste = opts.on_paste and 'callback' or nil,
-			on_close = 'callback',
 		}, function(event)
 			if event.type == 'activate' then
 				if (event.action == 'move_up' or event.action == 'move_down') and opts.on_move then
@@ -470,7 +469,6 @@ function open_file_navigation_menu(directory_path, handle_activate, opts)
 		title = opts.title or '',
 		footnote = t('%s to go up in tree.', 'alt+up') .. ' ' .. t('Paste path or url to open.'),
 		items = {},
-		on_close = opts.on_close and 'callback' or nil,
 		on_paste = 'callback',
 	}
 

commit 1cd34ae17630ccbfb7bc762635a964ac62e8e6f8

fix: menu crash when clicking on background introduced in 3bd8aef
closes #1027

src/uosc/elements/Menu.lua
@@ -150,7 +150,7 @@ end
 function Menu:destroy()
 	Element.destroy(self)
 	self:disable_key_bindings()
-	self.is_closed, self.is_closing, self.root, self.current, self.all, self.by_id = true, false, nil, nil, {}, {}
+	self.is_closed, self.is_closing = true, false
 	if not self.is_being_replaced then Elements:maybe('curtain', 'unregister', self.id) end
 	if utils.shared_script_property_set then
 		utils.shared_script_property_set('uosc-menu-type', nil)